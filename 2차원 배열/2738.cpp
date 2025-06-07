f39e6a39d930594feae0c049d62d06d1b3efde08
jaebin401 <anjaebin200701@gmail.com>
2025-06-07 18:47:03 +0900

    문제 초안 작성

---
+++ 2차원 배열/2738.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int row, col;
+    std::cin >> row >> col;
+    
+    std::vector<std::vector<int>> m(row, std::vector<int>(col, 0));
+    
+    for (int i{}; i<row; i++)
+    {
+        for (int j{}; j<col; j++)
+        {
+            std::cin >> m[i][j];
+        }
+    }
+    
+    for (int i{}; i<row; i++)
+    {
+        for (int j{}; j<col; j++)
+        {
+            int num;
+            std::cin >> num;
+            m[i][j] += num;
+        }
+    }
+    
+    for (int i{}; i<row; i++)
+    {
+        for (int j{}; j<col; j++)
+        {
+            std::cout << m[i][j];
+        }
+    }
+    
+    return 0;
+}
