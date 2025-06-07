f8bc8bf32801b404c40c6bd2885f80adc7630667
jaebin401 <anjaebin200701@gmail.com>
2025-06-07 20:44:28 +0900

    for문 2개 -> 1개로 입력 받자마자 대소비교 하는 알고리즘으로 수정 - 그래도 틀림

--- 2차원 배열/2566.cpp
+++ 2차원 배열/2566.cpp
@@ -6,27 +6,20 @@ int main()
     int N{9}, M{9};
     
     std::vector<std::vector<int>> m(N, std::vector<int>(M, 0));
-    
+    int max{}, Row{}, Col{};
+
     for (int row{}; row < N; row++)
     {
         for (int col{}; col < M; col++)
         {
             std::cin >> m[row][col];
-        }
-    }
-    
-    int max{}, Row{}, Col{};
-    for (int row{}; row < N; row++)
-    {
-        for (int col{}; col < M; col++)
-        {
+
             if (max < m[row][col])
             {
                 max = m[row][col];
                 Row = row+1;
                 Col = col+1;
             }
-            else { continue; }
         }
     }
     
