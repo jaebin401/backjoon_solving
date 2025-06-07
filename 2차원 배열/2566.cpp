fd9be865730dd2ff60be29164c092188b39417c4
jaebin401 <anjaebin200701@gmail.com>
2025-06-07 20:51:12 +0900

    2차원 배열로 입력 받을 필요도 없어서 그냥 입력 받는대로 비교하는 알고리즘으로 변경

--- 2차원 배열/2566.cpp
+++ 2차원 배열/2566.cpp
@@ -3,20 +3,19 @@
 
 int main()
 {
-    int N{9}, M{9};
     
-    std::vector<std::vector<int>> m(N, std::vector<int>(M, 0));
+    int element{};
     int max{}, Row{}, Col{};
 
-    for (int row{}; row < N; row++)
+    for (int row{}; row < 9; row++)
     {
-        for (int col{}; col < M; col++)
+        for (int col{}; col < 9; col++)
         {
-            std::cin >> m[row][col];
+            std::cin >> element;
 
-            if (max < m[row][col])
+            if (max < element)
             {
-                max = m[row][col];
+                max = element;
                 Row = row+1;
                 Col = col+1;
             }
