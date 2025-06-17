2b386f3a45a619cb2f90a1c77d8dc18187c72298
jaebin401 <anjaebin200701@gmail.com>
2025-06-17 13:01:16 +0900

    문제 초안 작성

---
+++ 2차원 배열/2563.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int area, squares, overlap;
+    struct coord
+    {
+        int x;
+        int y;
+    };
+    
+    std::cin >> squares;
+    std::vector<coord> m[squares];
+    area = 100*squares;
+    
+    for (int i{}; i<squares; ++i)
+    {
+        std::cin >> m[i].x >> m[y].y;
+    }
+    
+    for (int i{}; i<squares; ++i)
+    {
+        int basicX = m[i].x+10;
+        int basicY = m[i].y+10;
+        
+        for (int j{i+1}; j<squares; ++j)
+        {
+            if (basicX > m[j].x && basicY > m[j].y)
+            {
+                overlap += (m[j].x-m[i].x)*(m[j].y-m[i].x);
+            }
+        }
+    }
+    
+    area -= overlap;
+    std::cout << area << "\n";
+    
+    return 0;
+}
