fdcd80dbd10c4c19407f0501aeedd4082d2ced03
jaebin401 <anjaebin200701@gmail.com>
2025-06-24 18:57:07 +0900

    문제 초안 작성

---
+++ 수학1/2720.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    int unit[4] = {25, 10, 5, 1};
+    int T;
+    std::cin >> T;
+    
+    while (T--)
+    {
+        int N{};
+        int arr[4]{};
+        std::cin >> N;
+        
+        for (int i{}; i<4; i++)
+        {
+            while(N>unit[i])
+            {
+                arr[i]++;
+                N -= unit[i];
+            }
+        }
+        
+        for (int num : arr)
+            std::cout << num << " ";
+        
+        std::cout << "\n";
+    }
+    
+    return 0;
+}
