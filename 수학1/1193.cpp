922fb710bf58e93a906a0ad1a8db259435c5c58a
jaebin401 <anjaebin200701@gmail.com>
2025-06-29 16:00:46 +0900

    문제를 잘못 읽어서 재 풀이 함

--- 수학1/1193.cpp
+++ 수학1/1193.cpp
@@ -2,24 +2,38 @@
 
 int main()
 {
-    int row{}, col{}, nextRow{1}, n{}; 
+    int row{}, col{}, n{}, mode{};
+    
+    int nextRow[4] = {0, 1, 1, -1};
+    int nextCol[4] = {1, -1, 0, 1};
+    
     std::cin >> n;
+    n--;
     
     while(n--)
     {
-        if (row == 0)
-        {
-            row = nextRow;
-            nextRow++;
-            col = 0;
-            
-            continue;
-        }
+        row += nextRow[mode];
+        col += nextCol[mode];
         
-        row--; col++;
+        switch(mode)
+        {
+            case 0:
+            case 2:
+                {
+                    mode ++;
+                    continue;
+                }
+            case 1:
+                {
+                    if (col == 0) {mode++; continue;}
+                }
+            case 3:
+                {
+                    if (row == 0) {mode = 0; continue;}
+                }
+        }
     }
     
-    std::cout << row+1 << "/" << col+1 << "\n";
-    
+    std::cout << row+1 << "/" << col+1 << "\n"; 
     return 0;
 }
