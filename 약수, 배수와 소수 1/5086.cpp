ab560b71a1f9f53869885a5476ed59fd622cc5c9
jaebin401 <anjaebin200701@gmail.com>
2025-07-07 09:42:11 +0900

    변수 입력 코드 누락부분 추가

--- 약수, 배수와 소수 1/5086.cpp
+++ 약수, 배수와 소수 1/5086.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
     int a{}, b{};
+
+    cin >> a >> b;
     
     while(1)
     {
         if (a == 0 && b == 0) {break;}
-        
+
         if ( a % b == 0 ) cout << "multiple";
         else if ( b % a == 0 ) cout << "factor";
         else cout << "neither";  
