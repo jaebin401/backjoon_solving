c9603b237229118adc528e7b760855703cbb9bf4
jaebin401 <anjaebin200701@gmail.com>
2025-07-07 10:12:55 +0900

    문법, 논리 오류 수정 - 정답

--- 약수, 배수와 소수 1/2501.cpp
+++ 약수, 배수와 소수 1/2501.cpp
@@ -4,17 +4,17 @@
 int main()
 {
     int n{}, k{};
-    vector<int> L;
+    std::vector<int> L;
     
     std::cin >> n >> k;
     
-    for (int i{1}; i =< L; ++i)
+    for (int i{1}; i <= n; ++i)
     {
         if (n % i == 0) {L.insert(L.begin(), n/i);}
         else continue;
     }
     
-    if (k > L.length()) std::cout << 0;
+    if (k > L.size()) std::cout << 0;
     else std::cout << L[k-1];
     
     return 0;
